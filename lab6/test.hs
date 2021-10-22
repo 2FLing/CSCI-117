9e800f6acee6e6a90bd8bec6c4128b590fcae4dd
2FLing <349332929yaofu@gmail.com>
2021-10-21 17:11:34 -0700

    lab7整得我心态崩了

--- lab6/test.hs
+++ lab6/test.hs
@@ -1,6 +1,9 @@
 tst :: Int -> Int
 tst n
-  | n == 0 = 0
-  | n == 1 = 0
-  | n == 2 = 1
-  | otherwise = tst (n-2)+tst(n-1)+2
+  | n == 0 || n == 1 =0
+  | n == 2 = 2
+  | otherwise = help n where
+    help n
+      | n == 0 || n == 1 = 1
+      | n == 2 = 2
+      | otherwise = help (n - 1) + help (n - 2)+1
